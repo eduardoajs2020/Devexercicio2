57d2d86ef6cb9ea953eae73df0be30d78a6c9b8d
eduardoajs2020 <eduardoajs2020@gmail.com>
2020-11-13 22:06:14 -0300

    Desafio dev commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,44 @@
+import React from 'react';
+import './styles.scss';
+import {} from 'react-router-dom';
+
+
+const Home = () => (
+
+    <div className="superior-container">
+        <h4>Bootcamp DevSuperior</h4>
+
+        <div className="home-container">
+            <div className="row home-content card-base border-radius-20">
+                <div className="col-6 home-text">
+                    <h1 className="text-title">Desafio do Capitulo 3,<br />
+                        Bootcamp DevSuperior
+                    </h1>
+                    <div>
+                        <p className="text-subtitle">
+                                                    Bem vindos ao desafio do capitulo 3 do Bootcamp DevSuperior.<br />
+                            Favor Observar as instruções passadas no capítulo sobre a elaboração <br />
+                            deste projeto.
+                            Este design foi adaptado a partir de Art Design System for Figma, de <br />
+                            Mateusz Wierzbickl: antforfigma @gmail
+                        </p>
+                    </div>
+                   // <div className="button">
+                        <h4>começar</h4>
+                    </div>
+                    <div className="home-container">
+                        vazio
+                    </div>
+
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+
+
+
+);
+
+
